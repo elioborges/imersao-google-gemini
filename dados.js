8f5aba2e0b498014f6e1972090dbb9f105521f4f
Elioborges <34891443+elioborges@users.noreply.github.com>
2024-09-06 19:23:22 -0400

    imersão Google Gemini

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Lamborghini",
+        descricao: "Automobili Lamborghini S.P.A. é uma fabricante italiana de automóveis desportivos de luxo e de alto desempenho criada originalmente para competir com a Ferrari com sede no município de Sant'Agata Bolognese. A companhia, que foi fundada em 1963 por Ferruccio Lamborghini (1916–1993) como uma filial da sua bem-sucedida fábrica de tratores Lamborghini Trattori S.p.A., atualmente o grupo Volkswagen AG, a tem como subsidiaria da divisão Audi AG, onde intercambia tecnologias entre Audi R8 e os modelos mais recentes da Lamborghini.",
+        link: "https://pt.wikipedia.org/wiki/Lamborghini",
+        tags: "italiana ferrari desportivos"
+    },
+    {
+        titulo: "Camaro",
+        descricao: "O Chevrolet Camaro é um carro esportivo fabricado pela Chevrolet desde 1967. Originalmente concebido como um competidor para o Ford Mustang, o Camaro passou por várias gerações e se tornou um ícone da cultura pop americana.",
+        link: "https://pt.wikipedia.org/wiki/Chevrolet_Camaro",
+        tags: "Chevrolet amarelo"
+    },
+    {
+        titulo: "Fusca",
+        descricao: "O Fusca, também conhecido como Beetle, é um automóvel compacto do tipo besouro, originalmente produzido pela alemã Volkswagen. Lançado em 1938, o Fusca tornou-se um dos carros mais populares e produzidos em massa de todos os tempos.",
+        link: "https://pt.wikipedia.org/wiki/Fusca",
+        tags: "Volkswagen popular Beetle"
+    }
+];
+
